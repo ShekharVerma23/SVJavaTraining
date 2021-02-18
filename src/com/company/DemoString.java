8120dd0bf4d755b985a9af7a48c77d876bdbcbfb
Shekhar Verma <shverma@Shekhars-MacBook-Pro.local>
2021-02-19 00:57:22 +0530

    String Tutorial

---
+++ src/com/company/DemoString.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class DemoString {
+
+    public static void main(String [] args){
+        String first_Name = "Shekhar";
+        String last_Name = "Verma";
+
+        int firstName_Len = first_Name.length();
+        int lastName_Len = last_Name.length();
+
+        //use of for loop
+        for(int i=0; i<firstName_Len; i++){
+            System.out.print(first_Name.charAt(i));
+        }
+        System.out.println();
+
+        //use of while loop
+        int j=0;
+        while(j<lastName_Len){
+            System.out.print(last_Name.charAt(j));
+            j++;
+        }
+        System.out.println();
+
+        //concat
+        System.out.println("Concatenated Name is " + first_Name.concat(last_Name));
+
+        //lowercase
+        System.out.println("Name in lowercase is " + first_Name.toLowerCase() + " " + last_Name.toLowerCase());
+
+        //uppercase
+        System.out.println("Name in uppercase is " + first_Name.toUpperCase() + " " + last_Name.toUpperCase());
+    }
+}
